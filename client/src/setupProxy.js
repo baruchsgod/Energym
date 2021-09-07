bd58fb81a69b3e49239fbc8acff0a7cfbd56b39c
maiky.rodriguez <baruchsgod@hotmail.com>
2021-09-06 22:00:50 -0600

    Adding minor changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/auth',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      // target: 'http://localhost:3001',
+      target:'https://energymproject.herokuapp.com',
       changeOrigin: true,
     })
   );
