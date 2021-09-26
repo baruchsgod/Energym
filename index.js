57eab68891554ca7c299cd254bfdc7970ad6e220
maiky.rodriguez <baruchsgod@hotmail.com>
2021-09-26 15:24:52 -0600

    Removing lines from session in back-end

--- index.js
+++ index.js
@@ -36,11 +36,7 @@ app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
-    // cookie:{
-    //     sameSite:"none",
-    //     secure:true,
-    //     maxAge: 1000*60*60*24*7 //One week
-    // }
+   
 }));
 app.use(passport.initialize());
 app.use(passport.session());
