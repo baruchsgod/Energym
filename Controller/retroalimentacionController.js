5204ddb03fe546327e964fd0b081d9d2a7289240
Mario Sanchez <mcn_09@live.com>
2021-09-30 14:07:13 -0600

    Prueba ruta

--- Controller/retroalimentacionController.js
+++ Controller/retroalimentacionController.js
@@ -30,6 +30,7 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
+    return res.status(200).send({ title: 'Exito!', message: 'El Mensaje fue creado correctamente!', icon: 'success' });
     //Esta funcion agrega un Mensaje a la coleccion
     var retroalimentacion = new Retroalimentacion({
         "Cliente.Id": req.user._id,
