d2877b8429febf0244d1a4f006ba3b946475027c
Mario Sanchez <mcn_09@live.com>
2021-10-02 16:41:41 -0600

    test

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ app.use((req, res, next) => {
 });
 passport.use(User.createStrategy());
 passport.serializeUser(function (user, done) {
-    done(null, user.id);
+    done(null, user._id);
 });
 passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
         done(err, user);
-        console.log("deserialize " + user);
+        //console.log("deserialize " + user);
     });
 });
 
